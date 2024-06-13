0870be5316f4c59eaa659012390f7ee479d87bea
Soumya Sephali Pradhan <p.soumyasephali@gmail.com>
2024-06-13 19:00:29 +0530

    modified solution

--- Array/TwoSum.cpp
+++ Array/TwoSum.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
+
+//BruteForce Approach
+//Time complexity: O(n*n)
 
 vector<int>twoSum(vector<int>& nums, int target){
     int n = nums.size();
@@ -15,19 +17,65 @@ vector<int>twoSum(vector<int>& nums, int target){
     return {};
 }
 
-int main(){
-    
-    vector<int>arr = {2, 7, 11, 15};
-    int target = 9;
-    vector<int>result = twoSum(arr, target);
 
-    if(!result.empty()){
-        cout<<result[0]<<" "<<result[1]<<endl;
-    }
-    
-    else{
-        cout<<"No solution found!";
+//Sorting + Two Pointer Approach
+//Time complexity: O(nlogn)
+
+vector<int>twoSum(vector<int>& nums, int target){
+    int n = nums.size();
+
+    int a[n];
+    for(int i=0; i<n; i++){
+        a[i] = nums[i];
     }
 
-    return 0;
+    vector<int>v;
+    sort(nums.begin(), nums.end());
+
+    int l=0, r=n-1;
+    while(l<r){
+        if(nums[l] + nums[r] == target){
+            break;
+        }
+        else if(nums[l] + nums[r] < target){
+            l++;
+        }
+        else{
+            r--;
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        if(nums[i] == a[i]){
+            v.push_back(i);
+        }
+        else if(nums[r] == a[i]){
+            v.push_back(i);
+        }
+    }
+    return v;
+    
+}
+
+
+
+// //optimized Approach(using map)
+// //Time complexity: O(n)
+
+vector<int>twoSum(vector<int>& nums, int target){
+
+    int n = nums.size();
+    unordered_map<int, int>map;
+    vector<int>res;
+
+    for(int i=0; i<n; i++){
+        if(map.find(target - nums[i]) != map.end()){
+            res.push_back(map[target - nums[i]]);
+            res.push_back(i);
+            return res;
+        }else{
+            map[nums[i]] = i;
+        }
+    }
+    return res;
 }
